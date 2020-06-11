41282e5d6d9e7f6be87f59b9c7cc2292b943031d
Iyin Oluleke <iyinoluwa.olulek@gmail.com>
2020-06-11 13:54:45 +0100

    add create username, sending chat meessages and disconnect functionalities

--- app.js
+++ app.js
@@ -2,6 +2,7 @@ var express= require("express");
 var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
+var usernames = {};
 
 server.listen(process.env.port || 5000, function(){
 	console.log("App running");
@@ -12,8 +13,35 @@ app.get("/", function(req, res){
 });
 
 io.sockets.on("connection", function(socket){
-	//send message
-	socket.on("send message", function(data){
-		io.sockets.emit("new message",{msg:data});
+	socket.on("new user", function(userInput){
+		socket.emit("user-message", userInput);
+		usernames[socket.id] = userInput
+		socket.broadcast.emit("connectUser", userInput);
+	// 	// if(usernames.indexOf(data) != -1){
+	// 	// 	callback(false);
+	// 	// }else{
+	// 	// 	callback(true);
+	// 	// 	socket.username = data;
+	// 	// 	usernames.push(socket.username);
+	// 	// 	updateUsernames();
+	// 	// }
+
+	});
+	//update usernames
+	// function updateUsernames(){
+	// 	io.sockets.emit("usernames", usernames);
+	// }
+	//send message	
+	socket.on("send message", function(messageInput){
+		//console.log(messageInput);
+		socket.broadcast.emit("new message", {messageInput:messageInput, user:usernames[socket.id]});
+	});
+	//Disconnect
+	socket.on("disconnect", function(){
+		socket.broadcast.emit("disconnectUser", usernames[socket.id])
+		delete usernames[socket.id]
+		// if(!socket.username) return;
+		// usernames.splice(usernames.indexOf(socket.username), 1);
+		// updateUsernames();
 	});
 });
