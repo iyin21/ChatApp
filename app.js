1998d47147faeaf5a925ce5e8b27d220ff235c06
Iyin Oluleke <iyinoluwa.olulek@gmail.com>
2020-10-02 17:50:27 +0100

    Fix the bugs

--- app.js
+++ app.js
@@ -2,7 +2,7 @@ var express= require("express");
 var app = express();
 var server = require("http").createServer(app);
 var io = require("socket.io")(server);
-var usernames = {};
+var usernames = [];
 
 server.listen(process.env.port || 5000, function(){
 	console.log("App running");
@@ -13,32 +13,38 @@ app.get("/", function(req, res){
 });
 
 io.sockets.on("connection", function(socket){
-	socket.on("new user", function(userInput){
-		socket.emit("user-message", userInput);
-		usernames[socket.id] = userInput
-		socket.broadcast.emit("connectUser", userInput);
-	// 	// if(usernames.indexOf(data) != -1){
-	// 	// 	callback(false);
-	// 	// }else{
-	// 	// 	callback(true);
-	// 	// 	socket.username = data;
-	// 	// 	usernames.push(socket.username);
-	// 	// 	updateUsernames();
-	// 	// }
+	socket.on("new user", function(userInput, callback){
+		// socket.emit("user-message", userInput);
+		// usernames[socket.id] = userInput
+		// socket.broadcast.emit("connectUser", userInput);
+		if(usernames.indexOf(userInput) != -1){
+			callback(false);
+		}else{
+			callback(true);
+			socket.emit("user-message", userInput);
+			socket.username=userInput
+			usernames.push(socket.username)
+			socket.broadcast.emit("connectUser", userInput);
+			//usernames.push(socket.username);
+			updateUsernames();
+		}
 
 	});
 	//update usernames
-	// function updateUsernames(){
-	// 	io.sockets.emit("usernames", usernames);
-	// }
+	function updateUsernames(){
+		io.sockets.emit("users names", usernames);
+	}
 	//send message	
+	// io.sockets.emit("users names", function(data){
+	// 		appendUsername(data)
+	// 	}
 	socket.on("send message", function(messageInput){
 		//console.log(messageInput);
-		socket.broadcast.emit("new message", {messageInput:messageInput, user:usernames[socket.id]});
+		socket.broadcast.emit("new message", {messageInput:messageInput, user:socket.username});
 	});
 	//Disconnect
 	socket.on("disconnect", function(){
-		socket.broadcast.emit("disconnectUser", usernames[socket.id])
+		socket.broadcast.emit("disconnectUser", socket.username)
 		delete usernames[socket.id]
 		// if(!socket.username) return;
 		// usernames.splice(usernames.indexOf(socket.username), 1);
